f933e1043f4e98a219751dcf677258bd06017449
sbsumaiya65-coder <sbsumaiya65@gmail.com>
2026-09-14 02:44:37 +0600

    Refactor main.dart for route and theme adjustments
    Updated initial route and modified theme settings.

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,7 @@
-import 'package:flutter/material.dart';
+Import 'package:flutter/material.dart';
 import 'screens/auth_screen.dart';
-import 'screens/home_screen.dart';
-import 'screens/discover_screen.dart';
-import 'screens/chats_screen.dart';
-import 'screens/profile_screen.dart';
 import 'screens/main_navigation.dart';
-import 'screens/group_room_screen.dart';
-import 'screens/games_screen.dart';
 import 'screens/tictactoe_screen.dart';
-import 'screens/zone_feed_screen.dart';
-import 'coin_referral_manager.dart';
 
 void main() {
   runApp(const FriendsZoneApp());
@@ -21,24 +13,21 @@ class FriendsZoneApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       title: 'Friends Zone',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.pink,
-        useMaterial3: true,
+        scaffoldBackgroundColor: Colors.white,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.pink,
+          foregroundColor: Colors.white,
+        ),
       ),
-      initialRoute: '/',
+      initialRoute: '/auth',
       routes: {
-        '/': (context) => const MainNavigation(),
         '/auth': (context) => const AuthScreen(),
-        '/home': (context) => const HomeScreen(),
-        '/discover': (context) => const DiscoverScreen(),
-        '/chats': (context) => const ChatsScreen(),
-        '/profile': (context) => const ProfileScreen(),
-        '/group_room': (context) => const GroupRoomScreen(),
-        '/games': (context) => const GamesScreen(),
+        '/': (context) => const MainNavigation(),
         '/tictactoe': (context) => const TicTacToeScreen(),
-        '/zone_feed': (context) => const ZoneFeedScreen(),
       },
     );
   }
