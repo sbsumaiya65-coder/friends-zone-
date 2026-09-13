24ecdc52a15163c13c0ba07e80176ff660b5ae61
sbsumaiya65-coder <sbsumaiya65@gmail.com>
2026-09-14 02:28:05 +0600

    Add ChatsScreen with basic UI layout

---
+++ lib/screens/chats_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ChatsScreen extends StatelessWidget {
+  const ChatsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Chats')),
+      body: const Center(
+        child: Text('Your Conversations'),
+      ),
+    );
+  }
+}
