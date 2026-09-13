144702c0335ac6adeb5891b026ce125aeded7175
sbsumaiya65-coder <sbsumaiya65@gmail.com>
2026-09-14 01:52:15 +0600

    Update versionCode and versionName retrieval methods

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.example.friends_zone"
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = flutterVersionCode.toInteger()
-        versionName = flutterVersionName
+        versionCode = flutter.versionCode.toInt()
+        versionName = flutter.versionName
 
         ndk {
             abiFilters.addAll(setOf("armeabi-v7a", "arm64-v8a"))
