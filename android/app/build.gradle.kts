d6a9e86fa68399cf65426e1e28da4778fbdf4d35
sbsumaiya65-coder <sbsumaiya65@gmail.com>
2026-09-14 01:45:18 +0600

    Update build.gradle.kts for Android app configuration

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,3 +1,23 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("dev.flutter.flutter-gradle-plugin")
+}
+
+android {
+    namespace = "com.example.friends_zone"
+    compileSdk = flutter.compileSdkVersion
+    ndkVersion = flutter.ndkVersion
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+
     defaultConfig {
         applicationId = "com.example.friends_zone"
         minSdk = flutter.minSdkVersion
@@ -9,3 +29,14 @@
             abiFilters.addAll(setOf("armeabi-v7a", "arm64-v8a"))
         }
     }
+
+    buildTypes {
+        release {
+            signingConfig = signingConfigs.getByName("debug")
+        }
+    }
+}
+
+flutter {
+    source = "../.."
+}
